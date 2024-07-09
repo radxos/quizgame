b73b0a978eb5ad8f71b374e3494cef7230a7aff4
radwan <radwan@axos.systems>
2024-07-09 14:04:08 +1200

    added test file and learning about classes

---
+++ quiz.rb
@@ -0,0 +1,17 @@
+class Quiz
+    puts "#{"*" * 5} Welcome to Rad's Quiz Game! #{"*" * 5}"
+    puts "\n"
+
+    def score 
+        
+    end
+end
+
+class Questions
+    puts "Q: #{"Question Here"}"
+    puts "\n"
+end
+
+class Quiz 
+    puts "Editing Quiz class in another class instance"
+end
---
+++ quiz_test.rb
@@ -0,0 +1,45 @@
+require 'quiz'
+
+RSpec.describe Quiz do
+    describe "initial log" do
+        it "displays the welcome message" do
+
+        end
+
+        it "displays the first quiz question" do
+
+        end
+    end
+    
+    describe "correct answer" do
+        it "displays correct answer message" do
+
+        end
+
+        it "the score count increases by 1" do
+
+        end
+
+        it "displays the next question" do
+
+        end
+
+        it "displays the quiz end message and score after all answers submitted" do
+
+        end
+    end
+
+    describe "wrong answer" do
+        it "displays wrong answer message" do
+
+        end
+
+        it "the score count remains the same" do
+
+        end
+
+        it "displays the next question" do
+
+        end
+    end
+end
