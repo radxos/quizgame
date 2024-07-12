25fa3bd297859a5357cbddb123269b6ea3233f87
radwan <radwan@axos.systems>
2024-07-12 12:21:43 +1200

    indentation

--- quiz.rb
+++ quiz.rb
@@ -1,39 +1,38 @@
 class Quiz
-    def initialize
-        puts "#{'*' * 5} Welcome to Rad's Quiz Game! #{'*' * 5}"
-        puts "\n"
-    end
-
+  def initialize
+    puts "#{'*' * 5} Welcome to Rad's Quiz Game! #{'*' * 5}"
+    puts "\n"
+  end
 end
 
 class Score
-    def initialize
-        @score = 0
-    end
+  def initialize
+    @score = 0
+  end
 
-    def increase_score
-        @score += 1
-    end
+  def increase_score
+    @score += 1
+  end
 
-    def decrease_score
-        @score -= 1
-    end
+  def decrease_score
+    @score -= 1
+  end
 
-    def score
-        @score
-      end
+  def score
+    @score
+  end
 end
 
 class Questions
-    def initialize(question)
-      @question = question
-    end
-  
-    def display_question
-      puts "Q: #{@question}"
-      puts "\n"
-    end
+  def initialize(question)
+    @question = question
   end
+  
+  def display_question
+    puts "Q: #{@question}"
+    puts "\n"
+  end
+end
 
 quiz = Quiz.new
 
